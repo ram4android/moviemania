d22c08be0331e327be989c4012aca63411ba200e
Ram <balu434.a@gmail.com>
2018-10-08 21:40:08 +0530

    init commit

---
+++ Loginpage.kt
@@ -0,0 +1,12 @@
+package moviemania.ram.com.moviemania
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Loginpage : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loginpage)
+    }
+}
